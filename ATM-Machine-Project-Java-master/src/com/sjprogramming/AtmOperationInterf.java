1fa8fad2230c2b27d7f304101b0336094f3e3027
rohanks24 <rohanks2406@gmail.com>
2025-06-03 04:45:58 -0700

    Delete ATM-Machine-Project-Java-master directory

--- ATM-Machine-Project-Java-master/src/com/sjprogramming/AtmOperationInterf.java
+++
@@ -1,8 +0,0 @@
-package com.sjprogramming;
-
-public interface AtmOperationInterf {
-    public void viewBalance();
-    public void withdrawAmount(double withdrawAmount);
-    public void depositAmount(double depositAmount );
-    public  void viewMiniStatement();
-}
